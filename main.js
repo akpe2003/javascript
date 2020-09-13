d9a5341ccb5d9b7f563bf4c823e84b9aa9570620
akpe2003 <aes_akpe@yahoo.ca>
2020-09-14 00:51:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//const name="John";
+//const age=30;
+
+//console.log("My name is: " + name + " and am: " + age + "years of age");
+//console.log(`My name is ${name} and i am ${age}`);
+//const hello=`My name is ${name} and i am ${age}`;
+//console.log(hello);
+
+const name="Hello World";
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.substring(0,5));
+console.log(name.substring(0,5).toUpperCase());
+console.log(name.split(""));
