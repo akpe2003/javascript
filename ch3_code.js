49619937476bb4371c0216e2e80c679d2d3aedf3
akpe2003 <aes_akpe@yahoo.ca>
2020-09-16 12:39:44 +0100

    Add files via upload

---
+++ ch3_code.js
@@ -0,0 +1,18 @@
+var headingtext="<h1>A page of Java Script</h1>";
+var myintro="Hello, welcome to my JavaScript page!";
+var linktag="<a href=\"http://www.pageresource.com\">Link to a site</a>";
+var redtext="<span style=\"color:red\">I am so colorful today!</span>";
+var begineffect="<strong>";
+var endeffect="</strong>";
+var beginpara="<p>";
+var endpara="</p>";
+
+document.write(headingtext);
+document.write(begineffect+myintro+endeffect);
+document.write(beginpara);
+document.write(linktag);
+document.write(endpara);
+document.write(beginpara);
+document.write(redtext);
+document.write(endpara);
+//document.write();
