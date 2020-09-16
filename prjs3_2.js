4f7269611aa877681939f09ac363cdb8c0cd8dba
akpe2003 <aes_akpe@yahoo.ca>
2020-09-16 13:01:28 +0100

    Add files via upload

---
+++ prjs3_2.js
@@ -0,0 +1,16 @@
+var myheading="<h1>This is my web page</h1>";
+var sometext="This text can be affected by other statements.";
+var linktag="<a href=\"http://www.javascriptcity.com\">Website Link!</a>";
+
+var begineffect="<em>";
+var endeffect="</em>";
+var newsection="<br />"
+document.write(myheading);
+document.write(begineffect);
+document.write(sometext);
+document.write(endeffect);
+document.write(newsection);
+document.write(linktag);
+document.write(newsection);
+document.write(sometext);
+//document.write();
